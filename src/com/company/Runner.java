3a6fd87f3a010fe6311186105f9b006720da8e2c
rexuma1750 <43207408+rexuma1750@users.noreply.github.com>
2018-12-06 02:56:16 -0500

    Update Runner.java

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -11,16 +11,16 @@ public class Runner {
         face1.edit(" ",0,7);
         face1.edit(" "4,0);
 
-        face1.edit("",1,2);
-        face1.edit(" ",1,5);
-        face1.edit(" ",2,3);
-        face1.edit(" ",2,4);
-        face1.edit(" ",3,1);
-        face1.edit(" ",3,2);
-        face1.edit(" ",3,3);
-        face1.edit(" ",3,4);
-        face1.edit(" ",3,5);
-        face1.edit(" ",3,6);
+        face1.edit("0",1,2);
+        face1.edit("0 ",1,5);
+        face1.edit("^ ",2,3);
+        face1.edit("^ ",2,4);
+        face1.edit("| ",3,1);
+        face1.edit("| ",3,2);
+        face1.edit("- ",3,3);
+        face1.edit("-",3,4);
+        face1.edit("| ",3,5);
+        face1.edit("| ",3,6);
         System.out.println(face1);
     }
 
